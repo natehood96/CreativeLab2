bbc876774a159b182f99dbf16577ab81ec0b06ba
Nathan Hood <nhood@byu.edu>
2018-10-03 22:44:02 -0600

    trying commit and then import jason's files

--- form.js
+++ form.js
@@ -1,27 +1,49 @@
+var myPokemon = [
+    { "name": "", "pokeUrl": "" },
+];
+
+
+
 $(document).ready(function () {
-     $("#submitButton").click(function (e) {
-        var value; //= $("#cityField").val();
-        var squrUrl = "https://api.pokemontcg.io/v1/cards/bw8-24";
-        var charUrl = "https://api.pokemontcg.io/v1/cards/g1-rc3";
-         var bulbUrl = "https://api.pokemontcg.io/v1/cards/dp3-77"
-        console.log(myurl);
-        $.ajax({
-            url: myurl,
-            dataType: "json",
-            success: function (parsed_json) {
-                console.log(parsed_json);
-                var type = parsed_json.card.supertype;
-                var pokeUrl = parsed_json.card.imageUrl;
-                console.log(type);
-                $('#squr-image').attr('src', pokeUrl);
-            }
-        });
+    $("#pokeInput").click(function () {
+        var pokemonName = "#myselect option:selected".text();
+        console.log(pokemonName);
+        $("#submitButton").click(function (e) {
+            var value; //= $("#cityField").val();
+            //var myUrl;
+            var myUrl = "https://api.pokemontcg.io/v1/cards/bw8-24";
+            var charUrl = "https://api.pokemontcg.io/v1/cards/g1-rc3";
+            var bulbUrl = "https://api.pokemontcg.io/v1/cards/dp3-77"
+            console.log(myurl);
+            $.ajax({
+                url: myurl,
+                dataType: "json",
+                success: function (parsed_json) {
+                    console.log(parsed_json);
+                    var type = parsed_json.card.supertype;
+                    var pokeUrl = parsed_json.card.imageUrl;
+                    console.log(type);
+                    $('#squr-image').attr('src', pokeUrl);
+                    e.preventDefault();
+                }
+            });
         console.log(value);
         e.preventDefault();
         $("#displayCity").text(value);
+        });
     });
-});
 
+
+/*
+function populateSelects() {
+
+    var pokemon = $('#pokeInput');
+
+    myPokemon.forEach(function (item) {
+        pokemon.append($('<option></option>').attr('value', item.name).text(item.pokeUrl));
+    })
+}*/
+});
 
 /*
 $("#stackButton").click(function(e){
